6addb76fc61d98f6d16eb81b088103a057bc3004
Nathan Busse <bussenathan@icloud.com>
2024-11-27 11:16:42 +0200

    bug fix

--- nathan_website/Website/script.js
+++ nathan_website/Website/script.js
@@ -1,35 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Initialize Isotope for filtering
-    const isotope = new Isotope('.portfolio-grid', {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows',
-    })
-    
-   // Initialize Isotope for filtering
-   const Isotope = new Isotope('.certifications-grid', {
-    itemSelector: '.certifications-item',
-    layoutMode: 'fitRows'
-})
-
-// Filter buttons
-const filterButtons = document.querySelectorAll('.filter-btn');
-filterButtons.forEach(button => {
-    button.addEventListener('click', function () {
-        const filterValue = this.getAttribute('data-filter');
-        isotope.arrange({ filter: filterValue });
-
-        // Update active button
-        filterButtons.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
-});	
-
-    // Initialize Isotope for filtering
     const iso = new Isotope('.service-grid', {
         itemSelector: '.service-item',
-        layoutMode: 'fitRows'})
+        layoutMode: 'fitRows',});
 
     // Filter buttons
     const filterButtons = document.querySelectorAll('.filter-btn');
@@ -43,9 +17,11 @@ filterButtons.forEach(button => {
             this.classList.add('active');
         });
     });
-;	
+});
+
+
     // Smooth Scroll for Navbar Links
     document.querySelectorAll('.nav-links a').forEach(anchor => {
         anchor.add()
 	});
-;
+
