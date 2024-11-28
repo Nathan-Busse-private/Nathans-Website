7968bc15d840dc58a17bd327e4c56227e0f4964c
Nathan Busse <bussenathan@icloud.com>
2024-11-28 10:19:31 +0200

    Created a service navbar

--- nathan_website/Website/script.js
+++ nathan_website/Website/script.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+
 /* Service section */
     // Initialize Isotope for filtering
-    const iso = new Isotope('.service-grid', {
+    const service_isotope = new service_Isotope('.service-grid', {
         itemSelector: '.service-item',
         layoutMode: 'fitRows',});
 
     // Filter buttons
-    const filterButtons = document.querySelectorAll('.filter-btn');
-    filterButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const filterValue = this.getAttribute('data-filter');
-            iso.arrange({ filter: filterValue });
+    const service_filterButtons = document.querySelectorAll('.service-filter-btn');
+    service_filterButtons.forEach(button => {
+        button.addEventListener('service-click', function () {
+            const filterValue = this.getAttribute('.service-data-filter');
+            service_isotope.arrange({ filter: filterValue });
 
             // Update active button
-            filterButtons.forEach(btn => btn.classList.remove('active'));
-            this.classList.add('active');
+            service_filterButtons.forEach(btn => btn.classList.remove('service-active'));
+            this.classList.add('service-active');
         });
     });
 });
 
 
     // Smooth Scroll for Navbar Links
-    document.querySelectorAll('.nav-links a').forEach(anchor => {
+    document.querySelectorAll('.service-nav-links a').forEach(anchor => {
         anchor.add()
 	});
 
